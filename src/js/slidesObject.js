cc0ce1e90f656af64aa3e703b9ec6c9c2b4056eb
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-02-14 16:11:05 -0600

    build: slider component

---
+++ src/js/slidesObject.js
@@ -0,0 +1,30 @@
+const slides = [
+  {
+    id: "multidispositivo",
+    title: "Multidispositivo",
+    description: "Acceso desde cualquier dispositivo.",
+  },
+  {
+    id: "mobile_first",
+    title: "Mobile First",
+    description: "Prioriza el diseño para dispositivos móviles.",
+  },
+  {
+    id: "velocidad_carga",
+    title: "Velocidad de carga",
+    description: "Determina la relación del usuario con el sitio web.",
+  },
+  {
+    id: "optimizacion_seo",
+    title: "Optimización SEO",
+    description: "Aparecerás en las búsquedas de Google.",
+  },
+  {
+    id: "diseno_ui_ux",
+    title: "Diseño UI/UX",
+    description:
+      "Interfaces intuitivas y calidad en la experiencia del usuario.",
+  },
+];
+
+export default slides;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,js}"],
+  content: ["./src/**/*.{astro,js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
