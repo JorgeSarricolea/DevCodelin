66720b941fc97852da54997362cb86d36c18e8aa
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-02-14 11:21:57 -0600

    build: Hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,20 @@ module.exports = {
         primary: "#9ECDEF",
         secondary: "#0C1620",
         white: "#F0F0F0",
+        hover: "#86AAC4",
+      },
+      spacing: {
+        36: "36rem",
+        cursor: "0.25rem",
+      },
+      textShadow: {
+        lightBlue: "3px 3px 8px rgba(66, 135, 245, 1)",
+      },
+      boxShadow: {
+        lightBlue: "3px 3px 8px rgba(66, 135, 245, 1)",
       },
     },
   },
   variants: {},
-  plugins: [],
+  plugins: [require("tailwindcss-textshadow")],
 };
