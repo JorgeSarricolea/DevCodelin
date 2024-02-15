2347a32bb789729cfc1b320c21c013af0b6d8b52
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-02-14 19:00:20 -0600

    fix: slider component

--- src/js/slidesObject.js
+++ src/js/slidesObject.js
@@ -3,27 +3,32 @@ const slides = [
     id: "multidispositivo",
     title: "Multidispositivo",
     description: "Acceso desde cualquier dispositivo.",
+    icon: "fa-solid fa-network-wired",
   },
   {
     id: "mobile_first",
     title: "Mobile First",
     description: "Prioriza el diseño para dispositivos móviles.",
+    icon: "fa-solid fa-mobile-screen-button",
   },
   {
     id: "velocidad_carga",
     title: "Velocidad de carga",
     description: "Determina la relación del usuario con el sitio web.",
+    icon: "fa-solid fa-gauge-high",
   },
   {
     id: "optimizacion_seo",
     title: "Optimización SEO",
     description: "Aparecerás en las búsquedas de Google.",
+    icon: "fa-brands fa-searchengin",
   },
   {
     id: "diseno_ui_ux",
     title: "Diseño UI/UX",
     description:
       "Interfaces intuitivas y calidad en la experiencia del usuario.",
+    icon: "fa-solid fa-object-group",
   },
 ];
 
