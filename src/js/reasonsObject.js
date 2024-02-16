17362e5a7d5ea22fedaf211b8f390635e4a04746
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-02-16 14:11:06 -0600

    chore: add new reasons in reasonsObject

--- src/js/reasonsObject.js
+++ src/js/reasonsObject.js
@@ -15,6 +15,30 @@ const reasons = [
     image: "./public/img/adaptability.jpeg",
     position: "right",
   },
+  {
+    id: 3,
+    title: "Escalabilidad",
+    description:
+      "Tu proyecto puede comezar con un desarrollo simple e ir creciendo en función de las necesidades (añadir un blog, pasarela de pagos, un calendario…).",
+    image: "./public/img/escalability.jpeg",
+    position: "left",
+  },
+  {
+    id: 4,
+    title: "Interactividad con usuarios",
+    description:
+      "Sabemos que un buen producto/servicio es cuando el usuario está satisfecho con este, tener la posibilidad de hacer notificaciones por correo, comentarios no es problema con nosotros.",
+    image: "./public/img/interactivity.webp",
+    position: "right",
+  },
+  {
+    id: 5,
+    title: "Soporte & mantenimiento",
+    description:
+      "Un proyecto no termina con su publicación. Se trata de un produto dinámico que necesita soporte y mantenimiento para manter sus funcionalidades a pleno rendimiento.",
+    image: "./public/img/support.webp",
+    position: "left",
+  },
 ];
 
 export default reasons;
