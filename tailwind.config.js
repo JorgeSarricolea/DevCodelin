8daa2437bd1453f080b9f5df24b1dd43db0376ed
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-02-16 14:38:48 -0600

    style: add animation to Reasons component

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,20 @@ module.exports = {
       boxShadow: {
         lightBlue: "3px 3px 8px rgba(66, 135, 245, 1)",
       },
+      keyframes: {
+        slideInFromLeft: {
+          "0%": { transform: "translateX(-10%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+        slideInFromRight: {
+          "0%": { transform: "translateX(10%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+      },
+      animation: {
+        slideInLeft: "slideInFromLeft 1.5s ease-out forwards",
+        slideInRight: "slideInFromRight 1.5s ease-out forwards",
+      },
     },
   },
   variants: {},
