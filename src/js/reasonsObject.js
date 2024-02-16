16f5529a087ed1956ed765ed8e503b6834c88f86
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-02-15 22:15:06 -0600

    build: Reasons component

--- src/js/reasonsObject.js
+++ src/js/reasonsObject.js
@@ -1,18 +1,18 @@
 const reasons = [
   {
     id: 1,
-    title: "EMPATIA CON TU PROYECTO",
+    title: "Empatía con tu proyecto",
     description:
       "Nos ajustamos a tu presupuesto y a las necesidades de tu negocio, con el objetivo de hacer crecer tu marca y ventas, encaminándolo al éxito.",
-    image: "",
+    image: "./public/img/empathy.jpeg",
     position: "left",
   },
   {
     id: 2,
-    title: "ADAPTABILIDAD & FLEXIBILIDAD",
+    title: "Adaptabilidad & flexibilidad",
     description:
       "Utilizamos diferentes lenguajes de programación según las necesidades del proyecto hasta herramientas de desarrollo Low/No Code como WordPress, Bubble & FlutterFlow.",
-    image: "",
+    image: "./public/img/adaptability.jpeg",
     position: "right",
   },
 ];
