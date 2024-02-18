6c9408947f64b86ec2759fa4c7e575ca3c96c6f1
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-02-17 23:27:20 -0600

    build: Services component

---
+++ src/js/servicesObject.js
@@ -0,0 +1,22 @@
+const services = [
+  {
+    id: 1,
+    title: "Desarrollo del sistema web",
+    description:
+      "En DevGenie Studio, creamos sistemas web robustos y a medida, desde plataformas e-commerce con pasarelas de pago seguras hasta dashboards de administración intuitivos. Nuestra especialización abarca desde la creación de landing pages efectivas hasta la implementación de soluciones completas de e-commerce que impulsan la conversión y la gestión eficiente.",
+  },
+  {
+    id: 2,
+    title: "Tecnologías avanzadas y diseño responsivo",
+    description:
+      "No solo nos enfocamos en crear diseños web responsivos; en DevGenie Studio, llevamos la tecnología un paso adelante. Nuestro equipo está en la vanguardia, integrando las últimas innovaciones para desarrollar desde sitios web hasta soluciones avanzadas con inteligencia artificial o low/no code, diferenciándote claramente de tus competidores.",
+  },
+  {
+    id: 3,
+    title: "Metodologías ágiles y seo optimizado",
+    description:
+      "Impulsamos tu presencia en línea con estrategias SEO sólidas y nos adaptamos ágilmente a las dinámicas de tu proyecto. Utilizamos metodologías de trabajo ágiles como SCRUM para ofrecer flexibilidad, entregas periódicas y una mejora continua, asegurando que tu sitio web no solo sea visible, sino que también permanezca relevante y eficiente.",
+  },
+];
+
+export default services;
