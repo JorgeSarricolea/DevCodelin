515546dc6707cbb29f9890b895ec431c9b80a069
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2024-02-21 11:22:01 -0600

    fix: images location

--- src/js/reasonsObject.js
+++ src/js/reasonsObject.js
@@ -4,7 +4,7 @@ const reasons = [
     title: "Empatía con tu proyecto",
     description:
       "Nos ajustamos a tu presupuesto y a las necesidades de tu negocio, con el objetivo de hacer crecer tu marca y ventas, encaminándolo al éxito.",
-    image: "./public/img/empathy.jpeg",
+    image: "../img/empathy.jpeg",
     position: "left",
   },
   {
@@ -12,7 +12,7 @@ const reasons = [
     title: "Adaptabilidad & flexibilidad",
     description:
       "Utilizamos diferentes lenguajes de programación según las necesidades del proyecto hasta herramientas de desarrollo Low/No Code como WordPress, Bubble & FlutterFlow.",
-    image: "./public/img/adaptability.jpeg",
+    image: "../img/adaptability.jpeg",
     position: "right",
   },
   {
@@ -20,7 +20,7 @@ const reasons = [
     title: "Escalabilidad",
     description:
       "Tu proyecto puede comezar con un desarrollo simple e ir creciendo en función de las necesidades (añadir un blog, pasarela de pagos, un calendario…).",
-    image: "./public/img/escalability.jpeg",
+    image: "../img/escalability.jpeg",
     position: "left",
   },
   {
@@ -28,7 +28,7 @@ const reasons = [
     title: "Interactividad con usuarios",
     description:
       "Sabemos que un buen producto/servicio es cuando el usuario está satisfecho con este, tener la posibilidad de hacer notificaciones por correo, comentarios no es problema con nosotros.",
-    image: "./public/img/interactivity.webp",
+    image: "../img/interactivity.webp",
     position: "right",
   },
   {
@@ -36,7 +36,7 @@ const reasons = [
     title: "Soporte & mantenimiento",
     description:
       "Un proyecto no termina con su publicación. Se trata de un produto dinámico que necesita soporte y mantenimiento para manter sus funcionalidades a pleno rendimiento.",
-    image: "./public/img/support.webp",
+    image: "../img/support.webp",
     position: "left",
   },
 ];
